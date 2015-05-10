3dfa8a0cca2097cd6e1cd8f092fb8b491ccd516a
Camilo Ordonez <icoj@gmail.com>
2015-05-09 17:06:31 -0700

    Board set and marks working

--- app.js
+++ app.js
@@ -1,43 +1,78 @@
 // OOP Tic Tac Toe boilerplate code
+$(document).ready(function() { 
 
-// Execute this code only AFTER the document is ready
-// Hint: use jQuery's `$(document).ready`
+var turns = ["X","O","X","O","X","O","X","O","X"];
+var square;
+
 
   function Game() {
-    //Create a new instance of player 1
-    //this.player1 = ...
-
-    //Do the same for a player 2
-    //this.player2 = ...
-    
-    //Create the board
-    //this.board = ...
+    this.player1 = new Player("X");
+    this.player2 = new Player("O");
+    this.board = new Board();
   }
 
   // Remember: prototypes are shared functions between all game instances
   Game.prototype.nextPlayer = function() {
-    //Switch players
+      if (turns.length === 0) {
+        return false;
+      } else {
+        this.nextFigure = turns.pop();  
+      }  
   };
 
   // `Game.prototype.init` kicks off a new game with a board and two players
   Game.prototype.init = function() {
-    //
+    _this = this;
+    $('.row-ca').on('click',function(){
+      square = this;
+      _this.nextTurn();
+      _this.checkForWinner();
+    });
   };
+
+  Game.prototype.nextTurn = function(){
+      if (this.nextPlayer() === false) {
+        alert("Pailas");
+      } else {
+        $('.turn').text('Turn: ' + this.nextFigure);
+        this.board.markSquare(this.nextFigure);       
+      }
+  };
+
+  Game.prototype.checkForWinner = function(){
+    console.log("Yes");
+  };
+
 
   // A starter Player constructor.
   function Player(team) {
-    //Is the player X or O?
-    //this.team = ...
-  };
+    this.team = team;
+    this.marks = [];
+  }
 
   // A starter Board constructor.
   function Board() {
-    //Tracks the cells of the board instance
-    //this.$cells = ...
-
+    this.$cells = $('.row-ca');   
     //Store any other properties that board may have below, such as a reset option
+  }
+
+  Board.prototype.markSquare = function(team){
+    for(var i=0; i< this.$cells.length; i++){
+        if (this.$cells[i] === square) {
+            square.innerHTML = team;           
+        }
+    }
   };
+
+  
+
 
   // Start the game!
   var game = new Game();
   game.init();
+
+
+
+
+
+ });
